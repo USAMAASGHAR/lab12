7af568108907720f718264cc083fd9e0a4e6f27f
USAMAASGHAR <you@example.com>
2018-12-12 19:40:31 +0500

    intial code

---
+++ task1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+class shape{
+public:
+
+
+};
+class c : public shape {
+
+
+
+};
+class r : public shape {
+
+
+
+};
+class t : public shape {
+
+
+
+};
+
+
+int main()
+{
+
+  return 0;
+}
